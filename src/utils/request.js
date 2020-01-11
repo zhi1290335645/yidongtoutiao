5b640a3a6bfc4ae1f21d5ba5611dfac7e451b8ad
zhi1290335645 <793293398@qq.com>
2020-01-11 19:29:23 +0800

    发送token

--- src/utils/request.js
+++ src/utils/request.js
@@ -5,6 +5,10 @@
 import axios from 'axios'
 import jsonBig from 'json-bigint'
 
+// 在非组件模块中直接加载获取容器
+// 这里拿到的 store 和你在组件中访问 this.$store 是一个东西
+import store from '@/store'
+
 // axios.create 方法创建一个和 axios 本身功能一样的一个对象
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn/' // 基础路径
@@ -31,7 +35,34 @@ request.defaults.transformResponse = [function (data) {
 
 // 请求拦截器
 
+request.interceptors.request.use(function (config) {
+  // config 请求配置对象，我们可以通过修改 config 来实现统一请求数据处理
+  const { user } = store.state
+
+  // 统一添加 token
+  if (user) {
+    // config.headers 获取操作请求头对象
+    // Authorization 是后端要求的字段名称
+    // 数据值后端要求提供：Bearer token数据
+    //    注意：Bearer 后面有个空格
+    // 老师，为啥？后端要求的
+    config.headers.Authorization = `Bearer ${user.token}`
+  }
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
 // 响应拦截器
 
+request.interceptors.response.use(function (response) {
+  // Any status code that lie within the range of 2xx cause this function to trigger
+  // Do something with response data
+  return response
+}, function (error) {
+  // Any status codes that falls outside the range of 2xx cause this function to trigger
+  // Do something with response error
+  return Promise.reject(error)
+})
 // 导出
 export default request
